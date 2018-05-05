6159a0f2c7339c1ef9f03ce90a580809c00a5a68
JoelLindow <joellindow@gmail.com>
2018-05-05 14:14:20 -0600

    Nvbar installed. Links working. all pages linked

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'main#index'
 
-  get '/about', to: 'main#about'
   get '/main', to: 'main#index'
+  get '/about', to: 'main#about'
   get '/residential', to: 'main#gallery_residential'
   get '/commercial', to: 'main#gallery_commercial'
   get '/contact', to: 'main#contact'
