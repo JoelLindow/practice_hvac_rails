52c17d9e0dbfc74a39ac24681644cf02d555d241
erose357 <erose357@gmail.com>
2018-05-09 16:01:10 -0600

    Update mailer config for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,8 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.default_url_options = { :host => 'development-hvac.herokuapp.com' }
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
