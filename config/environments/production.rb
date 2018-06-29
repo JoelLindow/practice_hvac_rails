ac5381628d9c45eb96c55794b11a7ce1c9ca30c5
JoelLindow <joellindow@gmail.com>
2018-06-28 18:01:11 -0600

    fix mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.default_url_options = { :host => 'development-hvac.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'logichvacr.herokuapp.com' }
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
